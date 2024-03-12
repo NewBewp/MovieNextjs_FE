4995afbe843c46ef90675bb1502b352ca437ef00
hakien <nguyenhakien99@gmail.com>
2024-03-12 15:54:26 +0700

    validate register

--- src/components/auth/register/registerLayout.tsx
+++ src/components/auth/register/registerLayout.tsx
@@ -1,17 +1,24 @@
 "use client";
-import React, { useState } from "react";
-import { useForm } from "react-hook-form";
+
+import React from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { RegisterSchema, RegisterSchemaType } from "@/src/schema/RegisterSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 const RegisterLayout = () => {
-  const { register, handleSubmit, formState : {errors} } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors } } = useForm<RegisterSchemaType>({
+      mode: 'onChange',
+      resolver: zodResolver(RegisterSchema)
+    });
 
-  const onSubmit = (data: any) => {
-    console.log(data);
-    // Add logic to handle form submission
-
-
+  const onSubmit: SubmitHandler<RegisterSchemaType> = (RegisterValue) => {
+    console.log(RegisterValue);
+    // Add logic to handle form submission here
+    // Ví dụ: Gửi dữ liệu tới một API server
   };
-  console.log(errors);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
@@ -19,45 +26,87 @@ const RegisterLayout = () => {
 
       <div>
         <label htmlFor="first_name">First Name</label>
-        <input type="text" id="first_name"
-          {...register("first_name",{
-            required: 'Vui lòng nhập '
-          })} />
-          {/* {errors?.first_name && ()} */}
-        <p className="text-red-500">{errors?.first_name?.message as string}</p>
+        <input
+          type="text"
+          id="first_name"
+          {...register('first_name')}
+        />
+        {errors.first_name && <p className="text-red-500">{errors.first_name.message}</p>}
       </div>
 
       <div>
         <label htmlFor="last_name">Last Name</label>
-        <input type="text" id="last_name" {...register("last_name", { required: 'Vui lòng nhập' })} />
+        <input
+          type="text"
+          id="last_name"
+          {...register("last_name")}
+        />
+        {errors.last_name && <p className="text-red-500">{errors.last_name.message}</p>}
       </div>
 
       <div>
         <label htmlFor="phone_number">Phone Number</label>
-        <input type="text" id="phone_number" {...register("phone_number")} />
+        <input
+          type="text"
+          id="phone_number"
+          {...register("phone_number",
+            // {
+            //   required: 'Vui lòng nhập số điện thoại',
+            //   pattern: {
+            //     value: /^[0-9]+$/,
+            //     message: 'Chỉ nhập số'
+            //   }
+            // }
+          )}
+        />
+        {errors.phone_number && <p className="text-red-500">{errors.phone_number.message}</p>}
       </div>
 
       <div>
         <label htmlFor="email">Email</label>
-        <input type="text" id="email" {...register("email")} />
+        <input
+          type="email"
+          id="email"
+          {...register("email",
+            // {
+            //   required: 'Vui lòng nhập email',
+            //   pattern: {
+            //     value: /^\S+@\S+$/,
+            //     message: 'Định dạng email không hợp lệ'
+            //   }
+            // }
+          )}
+        />
+        {errors.email && <p className="text-red-500">{errors.email.message}</p>}
       </div>
 
       <div>
         <label htmlFor="address">Address</label>
-        <input type="text" id="address" {...register("address")} />
+        <input
+          type="text"
+          id="address"
+          {...register("address",
+            // { required: 'Vui lòng nhập địa chỉ' }
+          )}
+        />
+        {errors.address && <p className="text-red-500">{errors.address.message}</p>}
       </div>
 
       <div>
         <label htmlFor="gender">Gender</label>
-        <select name="gender" id="gender" >
-          <option value="male" {...register("gender")}>male</option>
-          <option value="female" {...register("gender")}>female</option>
+        <select id="gender" {...register("gender",
+          // { required: 'Vui lòng chọn giới tính' }
+        )}>
+          <option value="">--Chọn giới tính--</option>
+          <option value="male">Nam</option>
+          <option value="female">Nữ</option>
         </select>
+        {errors.gender && <p className="text-red-500">{errors.gender.message}</p>}
       </div>
 
       <button type="submit">Register</button>
     </form>
   );
-}
+};
 
 export default RegisterLayout;
---
+++ src/schema/RegisterSchema.tsx
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+export const RegisterSchema = z.object({
+    first_name: z.string().min(6, 'Nhập ít nhất 6 ký tự').max(20, 'Nhập tối đa 20 ký tự'),
+    last_name: z.string().min(6, 'Nhập ít nhất 6 ký tự'),
+    phone_number: z.string().min(1, 'Vui lòng nhập số điện thoại').max(20, 'Nhập tối đa 20 ký tự'),
+    email: z.string().min(1, 'Vui lòng nhập').email(),
+    address: z.string().min(1, 'Vui lòng nhập'),
+    gender: z.string().min(1, 'Vui lòng chọn').max(20, 'Nhập tối đa 20 ký tự'),
+})
+
+export type RegisterSchemaType = z.infer<typeof RegisterSchema>
